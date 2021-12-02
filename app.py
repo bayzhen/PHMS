b4a209c5ce1e389026d9d9d5cb278ae56fffa884
ZhangMister <35333375+ZhangMister@users.noreply.github.com>
2021-12-02 20:42:55 +0800

    part1 part2 part3

--- app.py
+++ app.py
@@ -1,18 +1,186 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, request, session,jsonify
+
+from db import connmysql
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+DB=connmysql(user="PHMS",password="123456",database="phms")
 
 @app.route('/')
 def hello_world():
-    return render_template("login.html")
+    return render_template("index.html")
 
-@app.route('/page1')
+@app.route('/register_D')
 def page1():
-    return render_template("page1.html")
+    return render_template("register_doctor.html")
 
-@app.route('/page2')
+@app.route('/register_P/')
 def page2():
-    return render_template("page2.html")
+    return render_template("register_patient.html")
+
+@app.route('/login/',methods=["POST"])
+def login():
+    username=request.values.get('username')
+    password=request.values.get('password')
+    identity=request.values.get('type')
+
+    sql="select * from %s where username='%s' and password='%s';"%(identity,username,password)
+    count_select=DB.select(sql)
+    if(count_select):
+        session['username']=username
+        session['password'] = password
+        session['identity']=identity
+        # 根据身份 跳转不同页面
+        if(identity=='patient'):
+            return render_template("PersonalHome.html",username=username)
+        elif(identity=='doctor'):
+            return render_template("DoctorHome.html",username=username)
+    else:
+        return render_template("index.html",message='Username or Password Error')
+
+    
+
+@app.route('/register_patient/',methods=["POST"])
+def register():
+    username=request.values.get('username')
+    password=request.values.get('password')
+    name = request.values.get('name')
+    gender=request.values.get('sex')
+    height = request.values.get('height')
+    weight = request.values.get('weight')
+    age=request.values.get('age')
+    phoneNum=request.values.get('phone')
+    job=request.values.get('job')
+    email=request.values.get('email')
+
+
+    sql="insert into patient(username,password,name,gender,height,weight,age,phoneNum,job,email) values('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');"%(username,password,name,gender,height,weight,age,phoneNum,job,email)
+    count_select=DB.update(sql)
+    return jsonify('OK')
+
+
+@app.route('/is_exist/',methods=["POST"])
+def is_exist():
+    username=request.values.get('username')
+    sql="select * from patient where username='%s';"%(username)
+    count_select=DB.select(sql)
+    valid=''
+    if(len(count_select)==0):
+        valid=True
+    else:
+        valid=False
+    return jsonify({"valid":valid})
+
+
+@app.route('/is_exist_doctor/',methods=["POST"])
+def is_exist_doctor():
+    username=request.values.get('username')
+    sql="select * from doctor where username='%s';"%(username)
+    count_select=DB.select(sql)
+    valid=''
+    if(len(count_select)==0):
+        valid=True
+    else:
+        valid=False
+    return jsonify({"valid":valid})
+
+@app.route('/register_doctor/',methods=["POST"])
+def register_doctor():
+    username=request.values.get('username')
+    password=request.values.get('password')
+    name = request.values.get('name')
+    gender=request.values.get('sex')
+    workID = request.values.get('workID')
+    department = request.values.get('department')
+    age=request.values.get('age')
+    phoneNum=request.values.get('phone')
+    major=request.values.get('major')
+    email=request.values.get('email')
+
+
+    sql="insert into doctor(username,password,name,gender,workID,department,age,phoneNum,major,email) values('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');"%(username,password,name,gender,workID,department,age,phoneNum,major,email)
+    count_select=DB.update(sql)
+    return jsonify('OK')
+
+
+@app.route('/')
+def DoctorHome():
+    return render_template('DoctorHome.html')
+
+@app.route('/PersonalHome/')
+def PersonalHome():
+    return render_template('PersonalHome.html')
+
+@app.route('/PatientsPage/')
+def index():
+    sql = "select * from patients;"
+    names=DB.select(sql)
+    data_dict={}
+    for i in range(0,len(names)):
+        data_dict[i+1]=list(names[i])
+    return render_template('PatientsPage.html',data_dict=data_dict)
+
+@app.route('/test1/',methods=["GET","POST"])
+def test1():
+    PatientName=request.args.get("name")
+    sql = "select * from patients where name='%s';" % (PatientName)
+    names = DB.select(sql)
+    data_dict = {}
+    for i in range(0, len(names)):
+        print(names[i])
+        data_dict[1 + i] = names[i]
+    return render_template('PatientsPage.html', data_dict=data_dict)
+
+@app.route('/test2/',methods=["GET","POST"])
+def test2():
+    name=request.args.get("Name")
+    age=request.args.get("Age")
+    HeartRate=request.args.get("HeartRate")
+    BloodPressure=request.args.get("BloodPressure")
+    BloodSugar= request.args.get("BloodSugar")
+    Cholesterol = request.args.get("Cholesterol")
+    GlycerinTrihydrate=request.args.get("GlycerinTrihydrate")
+    UricAcid = request.args.get("UricAcid")
+    Platelet = request.args.get("Platelet")
+    Glutopropase= request.args.get("Glutopropase")
+    sql="update patients set Age='%s', HeartRate='%s' ,BloodPressure='%s', BloodSugar='%s',Cholesterol='%s' ,GlycerinTrihydrate='%s',UricAcid='%s',Platelet='%s', Glutopropase='%s' where Name='%s';" %(age,HeartRate,BloodPressure,BloodSugar,Cholesterol,GlycerinTrihydrate,UricAcid,Platelet,Glutopropase,name)
+    DB.update(sql)
+
+    sql = "select * from patients;"
+    names = DB.select(sql)
+    data_dict = {}
+    for i in range(0, len(names)):
+        print(names[i])
+        data_dict[1 + i] = names[i]
+    return render_template('PatientsPage.html', data_dict=data_dict)
+
+@app.route('/PatientsEditPage/')
+def PatientsEditPage():
+    return render_template('PatientsEditPage.html')
+
+@app.route('/PersonalPage/')
+def PersonalPage():
+    sql = "select * from patients;"
+    names = DB.select(sql)
+    data_dict = {}
+    for i in range(0, len(names)):
+        data_dict[i + 1] = list(names[i])
+    return render_template('PersonalPage.html', data_dict=data_dict)
+
+
+@app.route('/test4/',methods=["GET","POST"])
+def test4():
+    PatientName=request.args.get("name")
+    sql = "select * from patients where name='%s';" % (PatientName)
+    names = DB.select(sql)
+    data_dict = {}
+    for i in range(0, len(names)):
+        print(names[i])
+        data_dict[1 + i] = names[i]
+    return render_template('PersonalPage.html', data_dict=data_dict)
+
 
 if __name__ == '__main__':
     app.run()
---
+++ db.py
@@ -0,0 +1,71 @@
+import pymysql,os
+# os:为系统模块，目的为正常退出
+# __init__ : 初始化属性
+# __del__ : 销毁对象时，自动执行该方法
+# __new__ : 实例化对象(创建一个对象)
+class connmysql(object):
+    obj = None   #对象第一次为None
+    def __new__(cls, *args, **kwargs): #单例模式
+        if cls.obj is None:     #如果对象为None
+            cls.obj = super().__new__(cls)  # 就创建一个对象
+
+        return cls.obj  #否则直接返回
+    def __init__(self,user,password,database,host="101.35.143.22",port=3306):
+        # 1. 链接数据库， 得到一个对象
+        try:
+            self.db=pymysql.connect(
+                host=host, port=port, user=user,  # 第一种
+                password=password, database=database, charset="utf8"
+            )
+        except Ellipsis as e:
+            print(e)
+            os._exit(0)   # 如果第一步的链接失败，下面的所有代码都不用走，直接退出
+
+        self.cs=self.db.cursor()   #2.得到一个游标对象
+
+    def select(self,sql):
+        """负责查询功能"""
+        #3 执行SQL语句
+        try:
+
+            self.cs.execute(sql)
+        except Exception as e:
+            print(e)
+            return
+        #4.获取查询结果
+        r=self.cs.fetchall()
+        return r    #将结果返回
+    def update(self,sql):
+        """负责更行功能"""
+        # 3 利用游标对象执行SQL语句
+        try:
+            self.cs.execute(sql)
+        except Exception as e:
+            print(e)
+            return
+        #4.提交 如果不进行提交，所有的改变(insert, delete, update)不会生效
+        self.db.commit()
+        print("更新成功！")
+
+
+    def __del__(self):
+        self.cs.close()  # 5. 关闭链接
+        self.db.close()
+
+
+
+# DB=connmysql(user="root",password="adsl12",database="assignment2")
+#
+# sql1="select id,place from data2;"
+# list=DB.select(sql1)
+# for item in list:
+#     distance=item[1].split('km')[0]
+#     if(len(distance)>3):
+#         distance=0
+#     else:
+#         distance=float(distance)
+#     print(distance)
+#     sql="UPDATE data2 SET distance=%f where id='%s';"%(distance,item[0])
+#     DB.update(sql)
+
+# var child="<tr><td>"+(i+1)+"</td><td>"+data[i][6]+"</td><td>"+data[i][2]+"</td><td>"+data[i][3]+"</td>"+"<td>"+"<button type='button' value="+data[i][5]+" onclick='selectByid(this)' class='btn btn-primary' data-toggle='modal' data-target='#myModal2' >edit</button>"+"</td>"+"</tr>";
